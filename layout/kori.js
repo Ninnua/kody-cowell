a73ef005f54c92ffb17c42f025f4339b9d173235
NimuaLilan <56268288+Ninnua@users.noreply.github.com>
2021-04-13 14:20:31 +0200

    Fixed sticky navbar

--- layout/kori.js
+++ layout/kori.js
@@ -21,13 +21,20 @@ function topFunction() {
 
 // ######################################### //
 
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 300) {
-        $('navbar').addClass('fixed');
-       
-    }
-    else {
-        $('navbar').removeClass('fixed');
-     
-    }
-});
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
